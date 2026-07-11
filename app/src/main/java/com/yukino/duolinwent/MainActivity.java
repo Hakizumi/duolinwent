69ff6bcb25cfca5de1d68e04f7e8a43b4ee0ff35
Hakizumi <Hakizumi@outlook.com>
2026-07-11 17:50:53 +0800

    refactor: Make `MainActivity` a `AppCompatActivity`

--- app/src/main/java/com/yukino/duolinwent/MainActivity.java
+++ app/src/main/java/com/yukino/duolinwent/MainActivity.java
@@ -1,11 +1,11 @@
 package com.yukino.duolinwent;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -14,7 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
  * The main activity page,
  * application main page.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
